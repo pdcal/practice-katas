b07ecb01b6c766875259f3d3fe05fc56ac9fda48
Philip Calvert <pdc@philipcalvert.co.uk>
2020-04-12 10:55:36 +0100

    Reordered folder structure, got pytest working

---
+++ diamond/tests/test_diamond.py
@@ -0,0 +1,6 @@
+# content of test_sample.py
+
+from diamond.app.diamond import *
+
+def test_answer():
+    assert func(3) == 4
--- tests/test_diamond.py
+++
@@ -1,6 +0,0 @@
-# content of test_sample.py
-
-from app.diamond import *
-
-def test_answer():
-    assert func(3) == 5
